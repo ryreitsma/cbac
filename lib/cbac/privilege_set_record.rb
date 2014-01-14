ec2eb166c02006b21ef65aa3a91bf94d7982705b
Roelof Reitsma <roelof.reitsma@ogd.nl>
2014-01-14 18:03:35 +0100

    Use attr_accessible for PrivilegeSetRecord#name
    This temporarily fixes the mass_assignment problems

--- lib/cbac/privilege_set_record.rb
+++ lib/cbac/privilege_set_record.rb
@@ -1,6 +1,8 @@
 class Cbac::PrivilegeSetRecord < ActiveRecord::Base
   self.table_name = "cbac_privilege_set"
 
+  attr_accessible :name
+
   def set_comment(comment)
     self.comment = comment if has_attribute?("comment")    
   end
